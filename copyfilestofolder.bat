fdee8c36253c50e732efeb28fd5ee0bfdd1b9ddd
jambalaya <jambalayadog77@gmail.com>
2022-11-20 09:55:04 -0800

    v0.1

---
+++ copyfilestofolder.bat
@@ -0,0 +1,3 @@
+robocopy I:\Projects\FantasyTools\FantasyTools\Reports\HockeyReports\GameReports I:\Projects\FantasyTools\FantasyTools\fantasytoolsapp\files
+echo files updated (probably!)
+
--- fantasytoolsapp/app.js
+++ fantasytoolsapp/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var scheduleRouter = require('./routes/schedule');
+var schedule2Router = require('./routes/schedule2');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'files')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/schedule', scheduleRouter);
+app.use('/schedule2', schedule2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
